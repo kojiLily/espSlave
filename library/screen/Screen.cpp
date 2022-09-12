9d3c5101edfac2f3dceda360565612809b7b4788
thinkpad_lily <19ec037@ms.dendai.ac.jp>
2022-09-12 18:06:41 +0900

    add screen code

--- library/screen/Screen.cpp
+++ library/screen/Screen.cpp
@@ -20,3 +20,29 @@ byte Screen::getIntervalTime()
     return this->intervalTime;
 }
 
+//描写パターン1
+void Screen::designPart1()
+{
+
+}
+//描写パターン2
+void Screen::designPart2()
+{
+    
+}
+//描写パターン3
+void Screen::designPart3()
+{
+    
+}
+//画面初期化
+void Screen::initScreen()
+{
+    epd.ClearFrame();
+}
+
+//画面の適用
+void Screen::applyScreen()
+{
+    epd.DispalyFrame();
+}
--- library/screen/Screen.h
+++ library/screen/Screen.h
@@ -7,14 +7,28 @@ class Screen
 {
 public:
     Screen();
+    Epd epd;
 
     //setter
     void setIntervalTime(byte setTime);
+    
 
     //getter
     byte getIntervalTime();    
 
+    //描写パターン
+    void designPart1();
+    void designPart2();
+    void designPart3();
+
+    //画面初期化
+    void initScreen();
+
+    //描写適用
+    void applyScreen();
+
 private:
     byte intervalTime; //画面の切り替えタイムを設定
+    bool screenRedesignOk;
 };
 #endif
