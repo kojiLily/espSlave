9ddebcba2dad1658cabd2ace0f14c43dc3d05c0b
Desktop_kojiLily <19ec037@ms.dendai.ac.jp>
2022-09-19 14:37:37 +0900

    Add: imagedata file

---
+++ library/screen/imagedata.cpp
@@ -0,0 +1,10 @@
+#include "imagedata.h"
+#include <pgmspace.h>
+
+const unsigned char IMAGE_BLACK[] PROGMEM = { 
+    // 0x00, 0x00, 0x00, 0x00,...
+};
+
+const unsigned char IMAGE_RED[] PROGMEM = {
+    // 0x00, 0x00, 0x00, 0x00,...
+};
